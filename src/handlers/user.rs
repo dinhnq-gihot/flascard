c424a9a1f837968f588ed5dfa8655914ef4da116
dinhnq-gihot <dinh.nguyen@gihot.vn>
2025-02-25 23:28:17 +0700

    feat: user handler

--- src/handlers/user.rs
+++ src/handlers/user.rs
@@ -1,1 +1,22 @@
+use {
+    crate::{db::db::Database, services::user::UserService},
+    anyhow::Result,
+    axum::{http::StatusCode, response::IntoResponse, Json},
+    std::sync::Arc,
+};
 
+pub struct UserHandler {
+    service: UserService,
+}
+
+impl UserHandler {
+    pub fn new(db: Arc<Database>) -> Self {
+        let service = UserService::new(db);
+        Self { service }
+    }
+
+    pub async fn get_all_users(&self) -> Result<impl IntoResponse> {
+        let users = self.service.get_all_users().await?;
+        Ok((StatusCode::OK, Json(users)))
+    }
+}
